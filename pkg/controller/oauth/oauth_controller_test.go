29bc1f8bba2a9f1712b033ae47bbdf275e7b60b3
James Harrington <james@harrington.net.au>
2021-12-15 15:25:24 -0500

    Fix tests after dep upgrade

--- pkg/controller/oauth/oauth_controller_test.go
+++ pkg/controller/oauth/oauth_controller_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/openshift/osd-metrics-exporter/pkg/metrics"
 
 	openshiftapi "github.com/openshift/api/config/v1"
@@ -84,20 +85,41 @@ func TestReconcileOAuth_Reconcile(t *testing.T) {
 			if tc.existingProviders == nil {
 				tc.existingProviders = make([]openshiftapi.IdentityProviderType, 0)
 			}
-			fakeClient := fake.NewFakeClientWithScheme(scheme.Scheme, makeTestOAuth(testName, testNamespace, tc.existingProviders...))
+
+			// Create OAuth Provider
+			// Initially create OAuth provider with or without Identity providers depending on test case
+			testOAuthCR := makeTestOAuth(testName, testNamespace, tc.existingProviders...)
+			fakeClient := fake.NewFakeClientWithScheme(scheme.Scheme, testOAuthCR)
 			reconciler := ReconcileOAuth{
 				client:            fakeClient,
 				metricsAggregator: metricsAggregator,
 			}
+
 			_, err = reconciler.Reconcile(reconcile.Request{
 				NamespacedName: types.NamespacedName{
 					Namespace: testNamespace,
 					Name:      testName,
 				},
 			})
+
+			// Reconcile shouldn't trigger any changes
 			require.NoError(t, err)
 
-			err = fakeClient.Update(context.Background(), makeTestOAuth(testName, testNamespace, tc.providers...))
+			var testOAuthCRAPI = &openshiftapi.OAuth{}
+
+			// After reconcile the CR metadata will change so Get it from the API, ensure the spec is still as we expect
+			err = fakeClient.Get(context.TODO(), types.NamespacedName{Name: testName, Namespace: testNamespace}, testOAuthCRAPI)
+			require.NoError(t, err)
+
+			if diff := cmp.Diff(testOAuthCR.Spec, testOAuthCRAPI.Spec); diff != "" {
+				t.Errorf("Expected OAuth .Spec to not change got %v want %v", testOAuthCRAPI.Spec, testOAuthCR.Spec)
+			}
+
+			// Modify IdentityProviderType list in OAuth
+			testOAuthCRAPI.Spec = makeTestOAuth(testName, testNamespace, tc.providers...).Spec
+
+			// Update the API
+			err = fakeClient.Update(context.Background(), testOAuthCRAPI)
 			require.NoError(t, err)
 
 			result, err := reconciler.Reconcile(reconcile.Request{
@@ -106,12 +128,14 @@ func TestReconcileOAuth_Reconcile(t *testing.T) {
 					Name:      testName,
 				},
 			})
+
+			// Validate our metrics reflect the changes to the OAuth IdentityProviderType list
 			// sleep to allow the aggregator to aggregate metrics in the background
 			time.Sleep(time.Second * 3)
 			require.NoError(t, err)
 			require.NotNil(t, result)
 			var testOAuth openshiftapi.OAuth
-			err = fakeClient.Get(context.Background(), types.NamespacedName{Name: "test", Namespace: "test"}, &testOAuth)
+			err = fakeClient.Get(context.Background(), types.NamespacedName{Name: testName, Namespace: testNamespace}, &testOAuth)
 			require.NoError(t, err)
 			require.Contains(t, testOAuth.ObjectMeta.Finalizers, finalizer)
 			metric := metricsAggregator.GetIdentityProviderMetric()
