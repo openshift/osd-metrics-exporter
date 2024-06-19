549f13b9abc6e221580c69c6b4bff59ae4bf1673
RaphaelBut <rbut@redhat.com>
2024-06-19 13:03:57 +0200

    replace dot imports in test files (typecheck errors)

--- controllers/machine/machine_suite_test.go
+++ controllers/machine/machine_suite_test.go
@@ -3,11 +3,11 @@ package machine
 import (
 	"testing"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
 )
 
 func TestMachine(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Machine Suite")
+	gomega.RegisterFailHandler(ginkgo.Fail)
+	ginkgo.RunSpecs(t, "Machine Suite")
 }
