aeda130adb1b5c3f1c58cedffee1cfbf5e68153c
RaphaelBut <rbut@redhat.com>
2024-06-19 13:03:57 +0200

    replace dot imports in test files (typecheck errors)

--- controllers/machine/machine_controller_test.go
+++ controllers/machine/machine_controller_test.go
@@ -3,27 +3,27 @@ package machine
 import (
 	"time"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-var _ = Describe("MachineController", func() {
+var _ = ginkgo.Describe("MachineController", func() {
 
-	Context("When Evaluating a deleting machine for blocking customer workloads", func() {
-		Context("When parsing the list of events firing on a cluster", func() {
-			Context("returns nil when no 'DrainRequeued' events are found", func() {
-				It("handles an empty event list", func() {
+	ginkgo.Context("When Evaluating a deleting machine for blocking customer workloads", func() {
+		ginkgo.Context("When parsing the list of events firing on a cluster", func() {
+			ginkgo.Context("returns nil when no 'DrainRequeued' events are found", func() {
+				ginkgo.It("handles an empty event list", func() {
 					eventList := &corev1.EventList{
 						Items: []corev1.Event{},
 					}
 					event, err := getMostRecentDrainFailedEvent(eventList)
-					Expect(err).Should(MatchError(errNoEvents))
-					Expect(event).To(BeNil())
+					gomega.Expect(err).Should(gomega.MatchError(errNoEvents))
+					gomega.Expect(event).To(gomega.BeNil())
 				})
-				It("handles an event list with no DrainRequeued events", func() {
+				ginkgo.It("handles an event list with no DrainRequeued events", func() {
 					eventList := &corev1.EventList{
 						Items: []corev1.Event{
 							{Reason: "DrainProceeds"},
@@ -32,12 +32,12 @@ var _ = Describe("MachineController", func() {
 					}
 
 					event, err := getMostRecentDrainFailedEvent(eventList)
-					Expect(err).To(BeNil())
-					Expect(event).To(BeNil())
+					gomega.Expect(err).To(gomega.BeNil())
+					gomega.Expect(event).To(gomega.BeNil())
 				})
 			})
 
-			It("Returns the newest event if no matter where in the list it is", func() {
+			ginkgo.It("Returns the newest event if no matter where in the list it is", func() {
 				newestTime := metav1.Now()
 				newerTime := metav1.NewTime(newestTime.Add(-5 * time.Minute))
 				oldestTime := metav1.NewTime(newerTime.Add(-5 * time.Minute))
@@ -59,45 +59,45 @@ var _ = Describe("MachineController", func() {
 				newestEventMiddle, err2 := getMostRecentDrainFailedEvent(newestMiddle)
 				newestEventLast, err3 := getMostRecentDrainFailedEvent(newestLast)
 
-				Expect(err1).To(BeNil())
-				Expect(err2).To(BeNil())
-				Expect(err3).To(BeNil())
+				gomega.Expect(err1).To(gomega.BeNil())
+				gomega.Expect(err2).To(gomega.BeNil())
+				gomega.Expect(err3).To(gomega.BeNil())
 
-				Expect(newestEventFirst).NotTo(BeNil())
-				Expect(newestEventMiddle).NotTo(BeNil())
-				Expect(newestEventLast).NotTo(BeNil())
+				gomega.Expect(newestEventFirst).NotTo(gomega.BeNil())
+				gomega.Expect(newestEventMiddle).NotTo(gomega.BeNil())
+				gomega.Expect(newestEventLast).NotTo(gomega.BeNil())
 
-				Expect(newestEventFirst.Message).To(ContainSubstring("Newest Drain Event"))
-				Expect(newestEventMiddle.Message).To(ContainSubstring("Newest Drain Event"))
-				Expect(newestEventLast.Message).To(ContainSubstring("Newest Drain Event"))
+				gomega.Expect(newestEventFirst.Message).To(gomega.ContainSubstring("Newest Drain Event"))
+				gomega.Expect(newestEventMiddle.Message).To(gomega.ContainSubstring("Newest Drain Event"))
+				gomega.Expect(newestEventLast.Message).To(gomega.ContainSubstring("Newest Drain Event"))
 			})
 		})
-		Context("When parsing the pod names and namespaces from an event", func() {
-			It("should return an empty map if there are no matches", func() {
+		ginkgo.Context("When parsing the pod names and namespaces from an event", func() {
+			ginkgo.It("should return an empty map if there are no matches", func() {
 				event := &corev1.Event{Message: "Should not match"}
 				pods := parsePodsAndNamespacesFromEvent(event)
 
-				Expect(pods).To(BeEmpty())
+				gomega.Expect(pods).To(gomega.BeEmpty())
 			})
-			It("should return the correct amount of matches for a single pod", func() {
+			ginkgo.It("should return the correct amount of matches for a single pod", func() {
 				event := &corev1.Event{Message: "pods/\"customer-pod\" -n \"test\" failed to drain"}
 				pods := parsePodsAndNamespacesFromEvent(event)
 
-				Expect(pods).To(HaveLen(1))
+				gomega.Expect(pods).To(gomega.HaveLen(1))
 			})
-			It("Should return the correct amount of matches if an openshift-pod is present", func() {
+			ginkgo.It("Should return the correct amount of matches if an openshift-pod is present", func() {
 				event := &corev1.Event{Message: "pods/\"osd-pod\" -n \"openshift-namespace\" does not exist; pods/\"customer-pod\" -n \"test\" failed to drain"}
 				pods := parsePodsAndNamespacesFromEvent(event)
 
-				Expect(pods).To(HaveLen(1))
+				gomega.Expect(pods).To(gomega.HaveLen(1))
 			})
-			It("Should return the correct amount of matches for multiple pods", func() {
+			ginkgo.It("Should return the correct amount of matches for multiple pods", func() {
 				event := &corev1.Event{Message: "pods/\"foo\" -n \"bar\" does not exist; pods/\"baz\" -n \"bat\" failed to drain"}
 				pods := parsePodsAndNamespacesFromEvent(event)
 
-				Expect(pods).To(HaveLen(2))
-				Expect(pods["foo"]).To(Equal("bar"))
-				Expect(pods["baz"]).To(Equal("bat"))
+				gomega.Expect(pods).To(gomega.HaveLen(2))
+				gomega.Expect(pods["foo"]).To(gomega.Equal("bar"))
+				gomega.Expect(pods["baz"]).To(gomega.Equal("bat"))
 			})
 		})
 	})
